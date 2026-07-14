7dea2cf8312815119abff01dd2b50c02fea758e5
19482939 <107518500+19482939@users.noreply.github.com>
2026-07-13 21:30:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
 function login() {
-  const password = document.getElementById("password").value;
-  const mensaje = document.getElementById("mensaje");
-
-  if (password === "1234") {
-    mensaje.innerHTML = "Acceso correcto ✅";
-  } else {
-    mensaje.innerHTML = "Contraseña incorrecta ❌";
-  }
-}
-
-// Registrar Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js");
-  });
+  alert("El botón funciona");
 }
