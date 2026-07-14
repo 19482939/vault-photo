76e70d74e800cb5064c69603763b5df523a6c332
19482939 <107518500+19482939@users.noreply.github.com>
2026-07-13 21:24:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+function login() {
+  const password = document.getElementById("password").value;
+  const mensaje = document.getElementById("mensaje");
+
+  if (password === "1234") {
+    mensaje.innerHTML = "Acceso correcto ✅";
+  } else {
+    mensaje.innerHTML = "Contraseña incorrecta ❌";
+  }
+}
+
 // Registrar Service Worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("service-worker.js")
-      .then(() => {
-        console.log("Service Worker instalado correctamente");
-      })
-      .catch((error) => {
-        console.log("Error al instalar Service Worker:", error);
-      });
+    navigator.serviceWorker.register("service-worker.js");
   });
 }
